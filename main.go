ef6abfe771347c1677d1a99635ae6aaed9266fd6
Kalpit Pant <kalpitpant@Kalpits-MacBook-Pro.local>
2022-01-22 23:53:35 +0530

    In memorydb first commit

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
 	"os"
 
 	"github.com/call-stack/inmemdb/internal"
 )
-
-func handleConnection(conn net.Conn, db *internal.Database) {
-	//do something here
-	scanner := bufio.NewScanner(conn)
-
-	for scanner.Scan() {
-
-		fmt.Println(scanner.Text())
-	}
-
-}
 
 func main() {
 	stop := make(chan os.Signal, 1)
